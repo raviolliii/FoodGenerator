e85672b5b879521b1a91c0bed43e062b22cd91b1
Ravi Patel <ravi.patel1245@gmail.com>
2017-06-08 08:21:31 -0400

    Updated script.js
    Updated script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-var mainfood = document.getElementById("mainItem");
-var yes = document.getElementById("yes");
-var no = document.getElementById("no");
-var vegbtn = document.getElementById("vegbtn");
 var page = 1;
 var pagelimit = 5;
 var foods = [];
 var link = "http://www.epicurious.com";
 var veg = false;
+var ingredients = [];
+var prep = [];
 
 function setFood() {
   let thing = Math.floor(Math.random() * foods.length);
   let title = foods[thing].getAttribute("title").toUpperCase();
   link = "http://www.epicurious.com" + foods[thing].getAttribute("href");
-  mainfood.innerHTML = title;
+  $("#mainfood").text(title);
 }
 
 function getFoods() {
@@ -43,23 +41,53 @@ function getFoods() {
 }
 getFoods();
 
-yes.addEventListener("click", function() {
-  window.open(link);
+function getrecipeinfo() {
+  var url = link;
+  var req = new XMLHttpRequest();
+  req.onload = function() {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(this.responseText, "text/html");
+    let items = doc.getElementsByClassName("ingredient");
+    ingredients = Array.prototype.slice.call(items);
+    let steps = doc.getElementsByClassName("preparation-step");
+    prep = Array.prototype.slice.call(steps);
+    console.log(prep);
+  };
+  req.open("GET", url, true);
+  req.send();
+}
+
+$("#yes").click(function() {
+  // window.open(link);
+  getrecipeinfo();
+  $("#top").animate({marginTop: "-60%"}, 250);
+  $("#ycontainer").animate({bottom: "-10%"}, 250);
+  $("#ncontainer").animate({bottom: "-10%"}, 250);
+  $("#vegbtn").animate({bottom: "-18%"}, 250);
+  $("#up").fadeIn("fast");
+  $("#up").animate({marginTop: "50%"}, 250);
 });
 
-no.addEventListener("click", function() {
+$("#no").click(function() {
   setFood();
 });
 
+$("#up").click(function() {
+  $("#top").animate({marginTop: "10%"}, 250);
+  $("#ycontainer").animate({bottom: "10%"}, 250);
+  $("#ncontainer").animate({bottom: "10%"}, 250);
+});
+
 vegbtn.addEventListener("click", function() {
-  mainfood.innerHTML = "HOLD UP ...";
+  $("#mainfood").text("HOLD UP ...");
   foods = [];
   page = 1;
   veg = !veg;
   if (veg) {
-    vegbtn.innerHTML = "WANT MEAT?";
+    $("#vegbtn").text("WANT MEAT?");
   } else {
-    vegbtn.innerHTML = "VEGETARIAN?";
+    $("#vegbtn").text("VEGETARIAN?");
   }
   getFoods();
 });
+
